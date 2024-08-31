211c81a199c3678a3175a9e19740cea2ae105dfe
marwa <marwa.bayood96@gmail.com>
2024-08-31 11:43:32 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function DarkModeFun(){
+    document.getElementById("ModeStyle").setAttribute("href","css/lightmode.css");
+}
+function lightModeFun(){
+    document.getElementById("ModeStyle").setAttribute("href","css/darkmode.css");
+}
+
